ff1904dff0e69cf5d2c944b08e1782f43926f57c
otiai10 <otiai10@gmail.com>
2015-02-04 08:19:04 +0900

    0.1.11

--- src/application/views/settings/comments-view.ts
+++ src/application/views/settings/comments-view.ts
@@ -7,7 +7,8 @@ module DMD {
                 comments:[
                     '<a href="https://chrome.google.com/webstore/detail/prisc/gghkamaeinhfnhpempdbopannocnlbkg?hl=ja">Prisc</a>入れてたら普通にスクショ撮れます',
                     'Chrome拡張設定画面(chrome://extensions)行って、いちばん下の「キーボードショートカット」を登録すればいけると思います',
-                    '[v0.1.10] 誤ってウィンドウ閉じるボタン押しちゃうときに対応しました'
+                    '[v0.1.10] 誤ってウィンドウ閉じるボタン押しちゃうときに対応しました',
+                    '[v0.1.11] 誤ってウィンドウ閉じるボタン押しちゃうときに対応を、せっていでon/off可能にしました'
                 ]
             }));
             return this;
