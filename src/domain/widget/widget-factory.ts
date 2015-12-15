10355002d3208f5cb2f5ce4d959f2c44e61a35bb
otiai10 <otiai10@gmail.com>
2015-12-15 15:10:50 +0900

    DELETE ALL

--- src/domain/widget/widget-factory.ts
+++
@@ -1,8 +0,0 @@
-
-module DMD {
-    export class WidgetFactory {
-        public static createDefault(): Widget {
-            return new Widget(new Size(800, 480), new Offset(75, 70), new Position(75, 70));
-        }
-    }
-}
