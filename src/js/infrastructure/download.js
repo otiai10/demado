90605f0449af2a3b55cfd903de9d2cdcb09bb326
otiai10 <otiai10@gmail.com>
2015-12-23 10:45:50 +0900

    Fix filename

--- src/js/infrastructure/download.js
+++ src/js/infrastructure/download.js
@@ -4,7 +4,7 @@ class Download {
   constructor(fname, opt, mod) {
     this.opt = opt || {
       filename: fname,
-      saveAs: true,
+      // saveAs: true,
       method: "GET"
     };
     this.mod = mod || chrome;
@@ -20,5 +20,20 @@ class Download {
         return res(id);
       });
     });
+  }
+  static zp(s, digit, padding) {
+    s = String(s);
+    digit = digit || 2;
+    padding = padding || "0";
+    for (var i = 0; i < digit; i++) {
+      s = padding + s;
+    }
+    return s.slice(s.length - digit);
+  }
+  static getFileName(prefix) {
+    var zp = Download.zp;
+    prefix = (prefix) ? prefix + "_" : "";
+    var d = new Date();
+    return prefix + d.getFullYear() + zp(d.getMonth() + 1) + zp(d.getDate()) + "_" + zp(d.getHours()) + zp(d.getMinutes());
   }
 }
--- src/js/server/controllers/capture-controller.js
+++ src/js/server/controllers/capture-controller.js
@@ -4,11 +4,12 @@ class CaptureController {
   static GetCapture(req, sender, sendResponse) {
     Capture.window().photo().then((res) => {
       ConfigStore.local().get("use-prisc").then((use) => {
-        if (!use) return Promise.reject();
+        if (!use.value) return Promise.reject();
         Message.to(Const.prisc).send("open/edit", {params: {imgURI: res.data}});
+        return Promise.resolve();
       }).catch((err) => {
         // download directly
-        var fname = "ファイル名" + "." + res.option.format;
+        var fname = Download.getFileName() + "." + res.option.format;
         Download.name(fname).save(res.data, true);
       });
     });
