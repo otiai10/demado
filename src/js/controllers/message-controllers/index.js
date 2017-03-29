555bb91e87274e184b9c3216aa21628bffca0c14
otiai10 <otiai10@gmail.com>
2017-03-29 04:10:19 +0300

    スクショの保存方法を変えた（が、saveAs;falseが効かないぞ）

--- src/js/controllers/message-controllers/index.js
+++ src/js/controllers/message-controllers/index.js
@@ -2,6 +2,8 @@ import {MadoConfigureManager} from '../../services/mado';
 import Mado from '../../models/Mado';
 import Launcher from '../../services/mado/Launcher';
 
+import Time from '../../services/time';
+
 export function MadoDelete({_id}) {
   const mado = Mado.find(_id);
   if (!mado) return {status:404, message:`Not found mado config for id ${_id}`};
@@ -112,11 +114,12 @@ export function MadoEntries() {
 export function MadoScreenshot({mado, winId}) {
   return new Promise(resolve => {
     chrome.tabs.captureVisibleTab(winId,{format: 'png'}, url => {
-      resolve({url});
-      let a = document.createElement('a');
-      a.href = url;
-      a.download = mado.name;
-      a.click();
+      const filename = `${mado.name}_${Time.new().xxx()}.png`;
+      chrome.downloads.download({url, filename}, resolve);
+      // let a = document.createElement('a');
+      // a.href = url;
+      // a.download = mado.name;
+      // a.click();
     });
   });
 }
