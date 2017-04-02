1cdb7a3cf1ba65f3ef964d1c46bf79922f149cdd
otiai10 <otiai10@gmail.com>
2017-04-02 13:14:58 +0900

    ショートカットキーでミュートを操作したときにダッシュボードのアイコンが変わらない問題の修正

--- src/js/container/dashboard.js
+++ src/js/container/dashboard.js
@@ -23,7 +23,10 @@ export default class DashboardView extends Component {
       <section className="section" style={{padding: '32px 16px 16px 16px'}}>
         <div className="container">
           <div className="columns">
-            {this.state.entries.map((entry, i) => <MadoEntryRow key={i} entry={entry} client={this.client} />)}
+            {this.state.entries.map((entry, i) => <MadoEntryRow key={i}
+                entry={entry} client={this.client}
+                refresh={this.refresh.bind(this)}
+            />)}
           </div>
         </div>
       </section>
