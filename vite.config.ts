9ac27b8c9b9c8d21cf9fc69a74ff7045e5358779
Hiromu OCHIAI <otiai10@gmail.com>
2023-09-15 13:43:13 +0900

    (just like) starting over (with vitejs again)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        page: 'src/pages/index.html',
+        background: 'src/background/worker.ts',
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
 })
