4f5eb879866a0009917de74bd53e34bd0d2aa520
Hiromu OCHIAI <otiai10@gmail.com>
2024-07-23 18:02:11 +0900

    OptionsのCSSの微修正

--- src/page/Options.tsx
+++ src/page/Options.tsx
@@ -5,15 +5,15 @@ export function OptionsPage() {
   const { mados } = useLoaderData() as { mados: Mado[] };
   return [
     <section className="section">
-      <div className="container">
+      <div className="container is-max-desktop">
         <p className="title">demadoの設定</p>
         <p className="subtitle is-size-6">任意のウェブページを小窓化できます. 詳しくは<a className="link" href="https://github.com/otiai10/demado/wiki" target="_blank">ここ</a></p>
       </div>
     </section>,
-    <section className="block">
-      <div className="container">
+    <section className="section">
+      <div className="container is-max-desktop">
         <div className="grid">
-          {mados.map((mado, i) => (
+          {mados.length === 0 ? <EmptyMadoEntryView /> : mados.map((mado, i) => (
             <div className="cell card demado-card" key={mado._id}
               style={{ borderColor: mado.colorcodeByIndex(i) }}
             >
@@ -37,21 +37,36 @@ export function OptionsPage() {
         </div>
       </div>
     </section>,
-    <section className="block">
-      <div className="container">
+    <section className="section">
+      <div className="container is-max-desktop">
         <div className="level">
           <div className="level-left">
             <div className="level-item">
-              <button className="button">新規追加</button>
+              <button className="button is-primary">
+                <i className="fas fa-plus"></i>
+                新規追加
+              </button>
             </div>
           </div>
           <div className="level-right">
             <div className="level-item">
-              <button className="button">削除</button>
+              <button className="button">すべて削除</button>
             </div>
           </div>
         </div>
       </div>
     </section>,
   ];
+}
+
+function EmptyMadoEntryView() {
+  return (
+    <div className="cell card demado-empty-state">
+      <div className="card-content">
+        <div className="content">
+          <p>まだ何も登録されていません</p>
+        </div>
+      </div>
+    </div>
+  );
 }
