891a24ed83308a03ebd446cb188641ea5e6d413d
Hiromu OCHIAI <otiai10@gmail.com>
2024-08-19 11:08:08 +0900

    権限付与サービスをリファクタした

--- src/services/PermissionService.ts
+++ src/services/PermissionService.ts
@@ -4,15 +4,14 @@ export default class PermissionService {
     private readonly mod: typeof chrome.permissions = chrome.permissions,
   ) { }
 
-  contains(url: string): Promise<boolean> {
+  private async contains(url: string): Promise<chrome.permissions.Permissions | null> {
     const urlObj = new URL(url);
-    return this.mod.contains({
-      origins: [urlObj.origin + "/*"], permissions: [
-        "activeTab",
-        "scripting",
-        "tabs",
-      ]
-    });
+    const perm = {
+      origins: [urlObj.origin + "/*"],
+      permissions: ["activeTab", "scripting", "tabs"],
+    };
+    const yes = await this.mod.contains(perm);
+    return yes ? perm : null;
   }
 
   public capture = {
@@ -20,19 +19,25 @@ export default class PermissionService {
     granted: () => this.mod.contains({ permissions: ["activeTab"], origins: ["<all_urls>"] }),
   }
 
-  request(url: string): Promise<boolean> {
+  private async request(url: string): Promise<chrome.permissions.Permissions | null> {
     const urlObj = new URL(url);
-    return this.mod.request({ origins: [urlObj.origin + "/*"] });
+    const perm = { origins: [urlObj.origin + "/*"] };
+    const yes = await this.mod.request(perm);
+    return yes ? perm : null;
   }
 
-  remove(url: string): Promise<boolean> {
-    const urlObj = new URL(url);
-    return this.mod.remove({ origins: [urlObj.origin + "/*"] });
-  }
+  // remove(url: string): Promise<boolean> {
+  //   const urlObj = new URL(url);
+  //   return this.mod.remove({ origins: [urlObj.origin + "/*"] });
+  // }
 
-  async ensure(url: string): Promise<boolean> {
-    const yes = await this.contains(url);
-    return yes ? yes : await this.request(url);
+  async ensure(url: string, fallback?: () => void): Promise<chrome.permissions.Permissions | void> {
+    let perm = await this.contains(url);
+    if (perm) return perm;
+    perm = await this.request(url);
+    if (perm) return perm;
+    if (fallback) return fallback();
+    throw new Error(`Permission denied for URL: ${url}`);
   }
 
 }
